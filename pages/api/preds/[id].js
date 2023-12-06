95452a90419d18a52fb1836b4dc914b195f3d400
sk-go <kaunsasha@proton.me>
2023-12-06 19:11:19 +0100

    First working version! �

--- pages/api/preds/[id].js
+++ pages/api/preds/[id].js
@@ -1,28 +1,20 @@
-export async function GET(req) {
-
-  if (!process.env.REPLICATE_API_TOKEN) {
-    return new Response("REPLICATE_API_TOKEN is not configured", { status: 500 });
+export default async function handler(req, res) {
+  const response = await fetch(
+    "https://api.replicate.com/v1/predictions/" + req.query.id,
+    {
+      headers: {
+        Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  if (response.status !== 200) {
+    let error = await response.json();
+    res.statusCode = 500;
+    res.end(JSON.stringify({ detail: error.detail }));
+    return;
   }
 
-  try {
-    const response = await fetch(
-      "https://api.replicate.com/v1/predictions/" + req.query.id,
-      {
-        headers: {
-          Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    if (response.status !== 200) {
-      let error = await response.json();
-      res.statusCode = 500;
-      res.end(JSON.stringify({ detail: error.detail }));
-      return;
-    }
-    const prediction = await response.json();
-    res.end(JSON.stringify(prediction));
-  } catch (error) {
-    console.error("Server error:", error);
-    return new Response("Internal Server Error", { status: 500 });
-}}
+  const prediction = await response.json();
+  res.end(JSON.stringify(prediction));
+}
